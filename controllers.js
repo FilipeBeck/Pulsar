2c0fdff5f99d88634fbfd7dfcd1c6ede1ba49801
Filipe Beck <Beck@Quasar.local>
2015-12-19 04:56:52 -0200

    Commit inicial

---
+++ controllers.js
@@ -0,0 +1,11 @@
+Pulsar.class('Controller', Object, function($)
+{
+	$('public var').storyboard = null;
+	
+	$('public var').navigationController = null;
+	
+	$('public').performSegue(identifier, sender)
+	{
+		
+	}
+})
